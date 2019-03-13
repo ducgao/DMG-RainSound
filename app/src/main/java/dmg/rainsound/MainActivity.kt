0f013b41db38ca19e113c0b1f1479ce5f7ed62ac
Nguyễn Văn Đức <ducgaogao4213@gmail.com>
2019-03-13 21:01:20 +0700

    complete

--- app/src/main/java/dmg/rainsound/MainActivity.kt
+++ app/src/main/java/dmg/rainsound/MainActivity.kt
@@ -4,23 +4,56 @@ import android.animation.Animator
 import android.animation.Animator.AnimatorListener
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.ViewPropertyAnimator
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
   private val btnStart: Button by lazy { findViewById<Button>(R.id.btnStart) }
   private val btnGoodNight: Button by lazy { findViewById<Button>(R.id.btnGoodNight) }
   private val ivBackground: ImageView by lazy { findViewById<ImageView>(R.id.ivBackground) }
+  private val adView: AdView by lazy { findViewById<AdView>(R.id.adView) }
 
   private var mediaPlayer: MediaPlayer? = null
+
+  private val adListener = object : AdListener() {
+    override fun onAdLoaded() {
+      println("DMG: add loaded")
+    }
+
+    override fun onAdFailedToLoad(errorCode: Int) {
+      println("DMG: add load failed with code $errorCode")
+      Handler().postDelayed({
+        adView.loadAd(AdRequest.Builder().build())
+      }, 5000)
+    }
+
+    override fun onAdOpened() {
+      println("DMG: add opened")
+    }
+
+    override fun onAdLeftApplication() {
+      println("DMG: add left application")
+    }
+
+    override fun onAdClosed() {
+      println("DMG: add closed")
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    MobileAds.initialize(this, "ca-app-pub-3656670221855991~8612734762")
 
     window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
@@ -41,6 +74,13 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  override fun onResume() {
+    super.onResume()
+
+    adView.adListener = adListener
+    adView.loadAd(AdRequest.Builder().build())
+  }
+
   private fun playSound() {
     val resID = resources.getIdentifier("sound", "raw", packageName)
     mediaPlayer = MediaPlayer.create(this@MainActivity, resID)
@@ -50,6 +90,8 @@ class MainActivity : AppCompatActivity() {
 
   override fun onDestroy() {
     super.onDestroy()
+
+    adView.adListener = null
 
     mediaPlayer?.stop()
     mediaPlayer?.release()
